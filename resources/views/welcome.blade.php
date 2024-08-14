01051bfba89a23021c6cb4fd5af2c16401e90691
mosesniyonk <mosesniyonk@gmail.com>
2024-08-14 08:47:21 +0000

    desc: .gitignore file commented

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,18 +20,19 @@
             <h2 class="flex-col gap-4 w-full flex items-center justify-center">
                 Show case your next ground breaking Ideas to the World.
             </h2>
-<div class="text-center">
-  <div
-    class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"
-  ></div>
-  <h2 class="text-zinc-900 dark:text-white mt-4">Loading...</h2>
-  <p class="text-zinc-600 dark:text-zinc-400">
-    Your adventure is about to begin
-  </p>
-</div>
+        </div>
+            <div class="text-center">
+                <div
+                    class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"
+                 ></div>
+                     <h2 class="text-zinc-900 dark:text-white mt-4">Loading...</h2>
+                    <p class="text-zinc-600 dark:text-zinc-400">
+                        Your adventure is about to begin
+                     </p>
+            </div>
 
-  </div>
-</div>
+
+        </div>
 
         </div>
     </body>
